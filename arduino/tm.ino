b5d524189b3deae0fcaa49d3fe7266d7ab25b90e
Michael Cerne <contact@michaelcerne.com>
2018-05-19 11:39:22 -0400

    Commented vehicle controller ino script

--- arduino/tm.ino
+++ arduino/tm.ino
@@ -1,3 +1,8 @@
+// Trucky McTruckface Mk.3 - 2018
+// Junior Solar Sprints
+// Michael Cerne
+// Motor/Lighting Controller
+
 int serIn, blinkt;
 bool blinkl, blinkr;
  
@@ -9,28 +14,28 @@ void setup() {
 void loop (){
   if(Serial.available()) {    
     while(Serial.available()>0){
-      serIn = Serial.read();  //read Serial        
+      serIn = Serial.read();  // Read serial        
       switch(serIn){
-        case '0':
+        case '0': // Stop Motor
           digitalWrite(8, LOW);
           digitalWrite(9, LOW);
           Serial.println("ln");
           break;
-        case '1':
+        case '1': // Start Motor (Forward)
           digitalWrite(9, LOW);
           digitalWrite(8, HIGH);
           break;
-        case '2':
+        case '2': // Start Motor (Reverse)
           digitalWrite(8, LOW);
           digitalWrite(9, HIGH);
           break;
-        case '3':
+        case '3': // Disable Lighting
           analogWrite(5, 0);
           break;
-        case '4':
+        case '4': // Dim Lighting
           analogWrite(5, 50); 
           break;
-        case '5':
+        case '5': // Full Lighting
           analogWrite(5, 255); 
           break;
       }
